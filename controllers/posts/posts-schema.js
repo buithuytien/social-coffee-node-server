906a1276c9bb821b778d6ca471ecd9880c9743e1
buithuytien <buithuytien13.13@gmail.com>
2022-12-11 22:21:06 -0500

    recipe external to Number

--- controllers/posts/posts-schema.js
+++ controllers/posts/posts-schema.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 const schema = mongoose.Schema({
     author: {type: mongoose.Schema.Types.ObjectId,  ref: 'UserModel'},
     recipe: {type: mongoose.Schema.Types.ObjectId, ref: 'RecipeModel'},
-    recipe_external: String,
+    recipe_external: {type: Number, unique: true},
     postImage: String,
     photos: [String], // remove ?
     title: String,
